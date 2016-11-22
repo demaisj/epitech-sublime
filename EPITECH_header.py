26bf53cc7a375111401c2ddde834cd9e2bd59c5d
Jordan Demaison <jordan.demaison@epitech.eu>
2016-11-22 02:16:15 +0100

    Better language recognition & more languages

--- EPITECH_header.py
+++ EPITECH_header.py
@@ -1,30 +1,91 @@
 import sublime, sublime_plugin
 import getpass, pwd, time, os, re
 
-supported_languages = ['c', 'cpp', 'h', 'hpp', 'mk', 'sh']
+supported_languages = [
+	'C',
+	'CSS',
+	'C++',
+	'Lisp',
+	'Shell-Unix-Generic',
+	'Makefile',
+	'Perl',
+	'Java',
+	'LaTeX',
+	'Pascal',
+	'Plain text',
+	'HTML',
+	'PHP',
+	'TeX',
+	'Python',
+	'JavaScript'
+]
 
 def get_syntax(ext):
-	if ext in ['c', 'cpp', 'h', 'hpp']:
+	if ext in ['C', 'CSS', 'C++', 'Java', 'JavaScript']:
 		return {
 			'comment_start': '/*',
 			'comment'      : '**',
 			'comment_end'  : '*/'
 		}
-	elif ext in ['mk', 'sh']:
+	elif ext in ['LaTeX', 'TeX']:
+		return {
+			'comment_start': '%%',
+			'comment'      : '%%',
+			'comment_end'  : '%%'
+		}
+	elif ext in ['Lisp']:
+		return {
+			'comment_start': ';;',
+			'comment'      : ';;',
+			'comment_end'  : ';;'
+		}
+	elif ext in ['Makefile', 'Plain text']:
 		return {
 			'comment_start': '##',
 			'comment'      : '##',
 			'comment_end'  : '##'
 		}
+	elif ext in ['Python']:
+		return {
+			'comment_start': '#!/usr/bin/env python\n##',
+			'comment'      : '##',
+			'comment_end'  : '##'
+		}
+	elif ext in ['Shell-Unix-Generic']:
+		return {
+			'comment_start': '#!/bin/bash\n##',
+			'comment'      : '##',
+			'comment_end'  : '##'
+		}
+	elif ext in ['Perl']:
+		return {
+			'comment_start': '#!/usr/bin/env perl\n##',
+			'comment'      : '##',
+			'comment_end'  : '##'
+		}
+	elif ext in ['Pascal']:
+		return {
+			'comment_start': '{',
+			'comment'      : '  ',
+			'comment_end'  : '}'
+		}
+	elif ext in ['HTML']:
+		return {
+			'comment_start': '<!--',
+			'comment'      : '  --',
+			'comment_end'  : '  -->'
+		}
+	elif ext in ['PHP']:
+		return {
+			'comment_start': '#!/usr/bin/env php\n<?php\n/*',
+			'comment'      : '**',
+			'comment_end'  : '*/'
+		}
 
-def get_ext():
-	var = sublime.active_window().extract_variables()
-	ext = var['file_extension']
-	if var['file_extension'] == "":
-		ext = "sh"
-	elif var['file_name'] == "Makefile":
-		ext = "mk"
-	return ext
+def get_language(self):
+	package = self.view.settings().get('syntax')
+	name = os.path.splitext(os.path.basename(package))[0]
+	return name
 
 class EpitechHeaderCommand(sublime_plugin.TextCommand):
 	header  = '{comment_start}\n'
@@ -41,9 +102,9 @@ class EpitechHeaderCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		global supported_languages
 
-		self.ext = get_ext()
+		self.ext = get_language(self)
 		if self.ext not in supported_languages:
-			return
+			sublime.message_dialog("Warning:\n\nThe current selected syntax is not supported by EPITECH_Sublime. We're adding one anyway.\nIf you want to add support for this syntax please report an issue in our GitHub repo.")
 		data = self.set_variables(edit)
 		self.view.insert(edit, 0, self.header.format(**data))
 		selection = self.view.sel()
@@ -80,7 +141,7 @@ class EpitechHeaderUpdateCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		global supported_languages
 
-		self.ext = get_ext()
+		self.ext = get_language(self)
 		if self.ext not in supported_languages:
 			return
 		syntax = get_syntax(self.ext)
